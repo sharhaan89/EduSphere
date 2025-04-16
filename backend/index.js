6fb2d8cb36cc5af3486c71680542a63e6a394d73
sharhaan89 <sksharhaannaim89@gmail.com>
2025-04-16 11:59:37 +0530

    threadedigt page updated

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,8 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use("/user", userRouter);
 app.use("/forum/thread", threadRouter);
